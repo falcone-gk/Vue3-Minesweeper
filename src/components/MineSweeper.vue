88baceca77c34819061af27cef05f03c28bc0d38
Enzo Falcón <falcone134@gmail.com>
2022-06-12 20:58:54 -0500

    Game menu done

--- src/components/MineSweeper.vue
+++ src/components/MineSweeper.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="gameboard" :style="gameboardStyle">
-    <div
-    v-for="index in store.getters.totalCells"
-    class="box"
-    :class="{shown: gameGrid[index-1].isShown, flagged: gameGrid[index-1].isFlagged}"
-    @click.prevent="propagationClick(index-1)"
-    @contextmenu.prevent="rightClick(index-1)"
-    :key="index">
-      <span v-if="gameGrid[index-1].isShown" :style="colorStyle(index-1)">{{ gameGrid[index-1].value }}</span>
+  <div class="game">
+    <div class="menu">
+      <div class="bar">
+        <div class="digital-number">
+          0{{ store.getters.getNumBombs }}
+        </div>
+        <div @click.prevent="setNewGame" class="reset">😃</div>
+        <div class="digital-number">
+          000
+        </div>
+      </div>
     </div>
+    <div class="gameboard" :style="gameboardStyle">
+      <div
+      v-for="index in store.getters.totalCells"
+      class="box"
+      :class="{shown: gameGrid[index-1].isShown, flagged: gameGrid[index-1].isFlagged}"
+      @click.prevent="propagationClick(index-1)"
+      @contextmenu.prevent="rightClick(index-1)"
+      :key="index">
+        <span v-if="gameGrid[index-1].isShown" :style="colorStyle(index-1)">{{ gameGrid[index-1].value }}</span>
+      </div>
+    </div>
+    <button @click.prevent="goToSetup">Configuración</button>
   </div>
-  <button @click.prevent="goToSetup">Configuración</button>
 </template>
 
 <script setup lang="ts">
@@ -35,7 +48,6 @@ const defaultCellState: cellState = {
 }
 const colors: string[] = ["#0000ff", "#008100", "#ff1300", "#000083", "#810500", "#2a9494", "#000000", "#808080"]
 
-const indexBombs: number[] = rangeRandomSample(store.getters.totalCells, store.getters.getNumBombs)
 const gameGrid = ref<cellState[]>(Array(numRows.value * numCols.value).fill(undefined).map(() => {
   // Using expansion when giving default object because when changing una key value it will
   // change every key with the same value, so we the solution is using expansion to make a copy.
@@ -96,6 +108,9 @@ const setNewGame = () => {
   return {...defaultCellState};
   })
 
+  // Getting new indexes for bombs when starting a new game
+  const indexBombs: number[] = rangeRandomSample(store.getters.totalCells, store.getters.getNumBombs)
+
   // Basically to hide every cell that has been shown and clean bombs index
   gameGrid.value = emptyGrid
   indexBombs.forEach((index) => {
@@ -125,28 +140,84 @@ setNewGame()
 
 </script>
 <style scoped>
+@import url('http://fonts.cdnfonts.com/css/digital-dismay');
+.game {
+  background-color: lightgray;
+  padding: 2px;
+}
+.menu {
+  display: flex;
+  box-sizing: border-box;
+  height: 60px;
+  background-color: #C0C0C0;
+  border: 1.5px solid;
+  border-top-color: #ffffff;
+  border-right-color: #7B7B7B;
+  border-bottom-color: #7B7B7B;
+  border-left-color: #ffffff;
+  text-align: center;
+  vertical-align: middle;
+}
+.bar {
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+  justify-content: space-between;
+  padding: 2px;
+  margin: 6px;
+  border-top: 2px solid #7B7B7B;
+  border-right: 2px solid #ffffff;
+  border-bottom: 2px solid #ffffff;
+  border-left: 2px solid #7B7B7B;
+}
+.reset {
+  text-align: center;
+  cursor: pointer;
+  font-size: 25px;
+  width: 35px;
+  height: 35px;
+  border-top: 2px solid #ffffff;
+  border-right: 2px solid #7B7B7B;
+  border-bottom: 2px solid #7B7B7B;
+  border-left: 2px solid #ffffff;
+}
+.reset:active {
+  background: #bdbdbd;
+  border: solid #999;
+  border-width: 2px 0 0 2px;
+}
+.digital-number {
+  font-family: 'Digital Dismay';
+  font-size: 30px;
+  letter-spacing: 1px;
+  color: red;
+  background-color: black;
+  text-align: center;
+  padding: 4px 4px 0 7px;
+  line-height: 1em;
+}
 .box {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 20px;
-    width: 20px;
-    background: #c0c0c0;
-    box-sizing: border-box;
-    border: solid #808080;
-    border-width: 0 1px 1px 0;
-    cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 30px;
+  width: 30px;
+  background: #c0c0c0;
+  box-sizing: border-box;
+  border: solid #808080;
+  border-width: 0 1px 1px 0;
+  cursor: pointer;
 }
 .box::after {
-    content: "";
-    display: block;
-    width: 100%;
-    height: 100%;
-    box-sizing: border-box;
-    background: #c0c0c0;
-    border: 2px outset #ececec;
-    font-size: 0.75rem;
-    text-align: center;
+  content: "";
+  display: block;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  background: #c0c0c0;
+  border: 2px outset #ececec;
+  font-size: 0.75rem;
+  text-align: center;
 }
 .flagged::after {
   content: "🚩";
@@ -155,8 +226,8 @@ setNewGame()
     display: none;
 }
 .box:active::after {
-    background: #bdbdbd;
-    border: solid #999;
-    border-width: 2px 0 0 2px;
+  background: #bdbdbd;
+  border: solid #999;
+  border-width: 2px 0 0 2px;
 }
 </style>
